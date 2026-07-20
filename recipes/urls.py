d9acbb993b49a6bbefe6470659de543faa7cca73
Lyn Smith <lynsmith5770@gmail.com>
2026-07-19 18:08:03 -0700

    class 2 assignment

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,27 @@
 from django.urls import path
+from .views import (
+    IngredientCreateView,
+    IngredientListView,
+    IngredientDetailView,
+    IngredientUpdateView,
+    IngredientDeleteView,
+    CategoryCreateView,
+    CategoryListView,
+    CategoryDetailView,
+    CategoryUpdateView,
+    CategoryDeleteView
+)
 
 urlpatterns = [
+    path('ingredients/', IngredientListView.as_view(), name='ingredients_list'),
+    path('ingredients/new/', IngredientCreateView.as_view(), name='ingredients_new'),
+    path('ingredients/detail/<int:pk>/', IngredientDetailView.as_view(), name='ingredients_detail'),
+    path('ingredients/edit/<int:pk>/', IngredientUpdateView.as_view(), name='ingredients_edit'),
+    path('ingredients/delete/<int:pk>/', IngredientDeleteView.as_view(), name='ingredients_delete'),
+    path('categories/', CategoryListView.as_view(), name='categories_list'),
+    path('categories/new/', CategoryCreateView.as_view(), name='categories_new'),
+    path('categories/detail/<int:pk>/', CategoryDetailView.as_view(), name='categories_detail'),
+    path('categories/edit/<int:pk>/', CategoryUpdateView.as_view(), name='categories_edit'),
+    path('categories/delete/<int:pk>/', CategoryDeleteView.as_view(), name='categories_delete'),
     
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,74 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+    )
+from .models import Category, Ingredient
+from django.urls import reverse_lazy
 
 # Create your views here.
+# Ingredients
+class IngredientListView(ListView):
+    model = Ingredient
+    template_name = 'ingredients/list.html'
+    context_object_name = 'ingredients'
+    # paginate_by = 10
+
+class IngredientDetailView(DetailView):
+    model = Ingredient
+    template_name = 'ingredients/detail.html'
+    context_object_name = 'ingredient'
+
+class IngredientCreateView(CreateView):
+    model = Ingredient
+    template_name = 'ingredients/new.html'
+    fields = ['name', 'description']
+    success_url = reverse_lazy('ingredients_list')
+
+class IngredientUpdateView(UpdateView):
+    template_name = 'ingredients/edit.html'
+    model = Ingredient
+    fields = ['name', 'description']
+    success_url = reverse_lazy('ingredients_list')
+
+class IngredientDeleteView(DeleteView):
+    template_name = 'ingredients/delete.html'
+    model = Ingredient
+    success_url = reverse_lazy('ingredients_list')
+
+# Categories
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'categories/list.html'
+    context_object_name = 'categories'
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'categories/detail.html'
+    context_object_name = 'category'
+
+class CategoryCreateView(CreateView):
+    model = Category
+    template_name = 'categories/new.html'
+    fields = ['name', 'description']
+    success_url = reverse_lazy('categories_list')
+
+class CategoryUpdateView(UpdateView):
+    template_name = 'categories/edit.html'
+    model = Category
+    fields = ['name', 'description']
+    success_url = reverse_lazy('categories_list')
+
+class CategoryDeleteView(DeleteView):
+    template_name = 'categories/delete.html'
+    model = Category
+    success_url = reverse_lazy('categories_list')
+
+
+
+
+
+
